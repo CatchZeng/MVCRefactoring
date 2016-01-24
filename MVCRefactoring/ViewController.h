073ad19acfb541dffc9160cc0dab07e4d8f47305
CatchZeng <891793848@qq.com>
2016-01-24 18:25:31 +0800

    Initial Commit

---
+++ MVCRefactoring/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVCRefactoring
+//
+//  Created by zengcatch on 16/1/24.
+//  Copyright © 2016年 catch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
